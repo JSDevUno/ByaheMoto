b2f33586dceea57422f4a700fb9da2f6d7ecd9d3
Jeevy Seguerra <jeevyseguerra6@gmail.com>
2024-09-03 15:15:01 +0800

    qwerty

---
+++ app/src/main/java/com/example/byahemoto/EditProfileDriver.kt
@@ -0,0 +1,11 @@
+package com.example.byahemoto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditProfileDriver : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_profile_driver)
+    }
+}
